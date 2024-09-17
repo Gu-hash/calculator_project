2b0e7006916afb7da3812afc044863f0f2e27aec
Gu-hash <gracegussher@gmail.com>
2024-09-17 10:50:33 +0100

    Added an entry box to display numbers which will be pressed

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import tkinter as tk
 frame = tk.Tk()
 frame.title("Calculator")
 
+#Creating an entry textbox
+#entry = tk.Entry(frame, width=40)
+#entry.pack(pady=10)
+textbox = tk.Text(frame, height=20, width=40)
+textbox.pack()
+
 frame.mainloop() #starts the event loop
