6619eb538c6dc02d51d5ed2857c873de40492053
Gu-hash <gracegussher@gmail.com>
2024-09-17 11:10:02 +0100

    Created grid to place widgets and set frame size

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import tkinter as tk
 #Creating the frame for the calculator
 frame = tk.Tk()
 frame.title("Calculator")
+frame.geometry("300x400")
 
 #Creating an entry textbox
-#entry = tk.Entry(frame, width=40)
-#entry.pack(pady=10)
-textbox = tk.Text(frame, height=20, width=40)
-textbox.pack()
+entry = tk.Entry(frame, width=50)
+entry.pack(pady=10)
+#textbox = tk.Text(frame, height=20, width=40)
+#textbox.pack()
 
+#Creating grid to place widgets
+entry.grid(rowspan=4, columnspan=4)
 frame.mainloop() #starts the event loop
