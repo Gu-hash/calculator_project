39687bc159639d9270312c7c40953af457ef38d9
Gu-hash <gracegussher@gmail.com>
2024-09-17 10:25:11 +0100

    added title to frame

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tkinter
-frame = tkinter.Tk()
+import tkinter as tk
 
-frame.mainloop()
+#Creating the frame for the calculator
+frame = tk.Tk()
+frame.title("Calculator")
+
+frame.mainloop() #starts the event loop
